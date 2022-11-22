61083ec7fe7aee08fa0e9d4c3c4d2dc2918dc047
Cindy-Ariel <cindyariel50@hotmail.com>
2022-11-21 23:11:16 -0300

    Inserido componente Produtos na pasta pages

--- fumagally_eventos/pages/index.tsx
+++ fumagally_eventos/pages/index.tsx
@@ -1,12 +1,14 @@
-import { Main } from "../src/components/Main";
+import { Main } from "../src/containers/Main";
 import { Header } from "../src/components/Header";
 import styles from "../styles/Home.module.css";
+import { Produtos } from "../src/containers/Products";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Main />
+      <Produtos/>
     </div>
   );
 }
