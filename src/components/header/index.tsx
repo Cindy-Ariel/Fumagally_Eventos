cd2c2fd74964b944f9a0df1eb6f239f472b9a6f7
Cindy <cindyariel50@hotmail.com>
2023-04-08 14:26:49 -0300

    Estruturando componente header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import * as S from "./style";
+
+export const Header = () => {
+  return (
+    <S.header>
+      <S.logo src="" alt="" />
+
+      <S.navegacao>
+        <S.ul>
+          <li>PROMOÇÕES</li>
+          <li>TOPICOS</li>
+          <li>SERVIÇOS</li>
+          <li>PERGUNTAS</li>
+          <li>EVENTOS</li>
+          <S.botao>CONTATO</S.botao>
+        </S.ul>
+      </S.navegacao>
+    </S.header>
+  );
+};
---
+++ src/components/header/style.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+// import logo from '../../../public/logo2'
+
+export const header = styled.header`
+background-color: #3A1484;
+width: 100vw;
+height: 100px;
+display: flex;
+justify-content: space-around;
+align-items: center;
+font-size: 0.8125rem;
+color: white;
+font-weight: 500;          
+
+`
+export const logo = styled.img`
+background-image: url('logo.png.jpg');
+width: 200px;
+height: 60px;
+`
+export const navegacao = styled.nav`
+background-color: aqua;
+width: 65%;
+height: 100%;
+display: flex;
+`
+
+export const ul =  styled.ul`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+align-items: center;
+background-color: #3A1494;
+list-style: none;
+
+`
+export const botao = styled.button`
+width: 100px;
+height: 35px;
+background-color: #FDD422;
+border-radius: 15px;
+
+`
